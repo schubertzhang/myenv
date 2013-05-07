67fd06b00be9c3686405ec421df36a38cfd45b78
Schubert Zhang <schubert.zhang@gmail.com>
2013-05-07 17:10:14 +0800

    initial commit for my vm, include hadoop, hbase, zookeeper, and linux

---
+++ vm/linux/myprofile.sh
@@ -0,0 +1,10 @@
+#JAVA JDK
+export JAVA_HOME=/usr/lib/jvm/jdk1.6.0_35
+export JRE_HOME=${JAVA_HOME}/jre
+export PATH=${JAVA_HOME}/bin:$PATH
+
+export ANT_HOME=/usr/local/ant
+export PATH=${ANT_HOME}/bin:$PATH
+
+
+alias dstat='dstat -cdlmnpsy'
