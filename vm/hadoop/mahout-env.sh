5a6245aec6dcd27af3a33b53ac45251a15fce33b
Schubert Zhang <schubert.zhang@gmail.com>
2013-07-19 18:17:43 +0800

    add mahout

---
+++ vm/hadoop/mahout-env.sh
@@ -0,0 +1,20 @@
+
+
+# The java implementation to use.  Overrides JAVA_HOME.
+export MAHOUT_JAVA_HOME=/usr/lib/jvm/jdk1.6.0_35
+
+# The maximum amount of heap to use, in MB. Default is 1000.
+export MAHOUT_HEAPSIZE=1000
+
+
+export HADOOP_PREFIX=/home/schubert/work/bigdata/hadoop-1.1.2
+#export HADOOP_HOME=$HADOOP_PREFIX
+
+
+# The location of a hadoop config directory
+export HADOOP_CONF_DIR=$HADOOP_PREFIX/conf
+
+
+export MAHOUT_HOME=/home/schubert/work/bigdata/mahout-distribution-0.7
+
+
